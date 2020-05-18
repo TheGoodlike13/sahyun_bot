9f398ccdff1710215b2ad4d12ad3119bca5755b2
TheGoodlike13 <henrisssss@hotmail.com>
2020-05-18 17:47:41 +0300

    Re-paralleled (hey, that's what the spelling was corrected into)

--- sahyun_bot/the_loaderer.py
+++ sahyun_bot/the_loaderer.py
@@ -1,19 +1,48 @@
 import logging
+from concurrent.futures.thread import ThreadPoolExecutor
 from datetime import datetime, timedelta
 
 from sahyun_bot.customsforge import CustomsForgeClient
 from sahyun_bot.elastic import last_auto_index_time, CustomDLC
 
-LOG = logging.getLogger('theload')
+BACKGROUND_WORKERS = ThreadPoolExecutor(6)
+
+
+LOG = logging.getLogger('theloaderer')
 
 
 def load(cf: CustomsForgeClient):
+    work_queue = []
+
     load_from = last_auto_index_time() or 0
     load_from_date = datetime.fromtimestamp(load_from).date() - timedelta(days=1)
     for cdlc in cf.cdlcs(since=load_from_date, since_exact=load_from):
-        c = CustomDLC(**cdlc)
-        LOG.info('Loading CDLC #%s: %s (by %s)', c.id, c.full_title, c.author)
-
-        c.from_auto_index = True
-        c.direct_download = cf.direct_link(c.id)
+        c = CustomDLC(from_auto_index=True, **cdlc)
         c.save()
+        work = BACKGROUND_WORKERS.submit(_load_direct_link, cf, c)
+        work_queue.append(work)
+
+    for work in work_queue:
+        work.result()
+
+
+def load_links(cf: CustomsForgeClient):
+    work_queue = []
+
+    for c in CustomDLC.search().exclude('exists', field='direct_download').scan():
+        work = BACKGROUND_WORKERS.submit(_load_direct_link, cf, c)
+        work_queue.append(work)
+
+    for c in CustomDLC.search().params(q='direct_download.keyword:""').scan():
+        work = BACKGROUND_WORKERS.submit(_load_direct_link, cf, c)
+        work_queue.append(work)
+
+    for work in work_queue:
+        work.result()
+
+
+def _load_direct_link(cf: CustomsForgeClient, c: CustomDLC):
+    LOG.debug('Updating link for CDLC #%s', c.id)
+    direct_link = cf.direct_link(c.id)
+    if direct_link:
+        c.update(direct_download=direct_link)
--- tests/test_the_loaderer.py
+++ tests/test_the_loaderer.py
@@ -2,7 +2,7 @@ from assertpy import assert_that
 from httmock import HTTMock
 
 from sahyun_bot.elastic import CustomDLC
-from sahyun_bot.the_loaderer import load
+from sahyun_bot.the_loaderer import load, load_links
 from tests.mock_customsforge import customsforge
 
 
@@ -27,3 +27,17 @@ def test_loading_continued(cf, es_fresh):
 
     for hit in hits:
         assert_that(hit.link).is_equal_to('magical_link')
+
+
+def test_load_links(cf, es_fresh):
+    # TODO: this simulates the scenario where link was empty due to error - does not work because ES was miss-configured
+    # CustomDLC.get(49841).update(direct_download='')
+
+    print(CustomDLC.get(49841).to_dict())
+
+    with HTTMock(customsforge):
+        load_links(cf)
+
+    assert_that([hit.link for hit in CustomDLC.search()])\
+        .is_length(6)\
+        .contains_only('magical_link')
