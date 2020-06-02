c9203acb036475a3ea8120446e78e0d319666ae1
TheGoodlike13 <henrisssss@hotmail.com>
2020-06-02 19:06:19 +0300

    Implemented !random, refactored common request logic

--- sahyun_bot/commands/request_queue.py
+++ sahyun_bot/commands/request_queue.py
@@ -4,7 +4,8 @@ Commands which control the request queue.
 
 from __future__ import annotations
 
-from typing import List, Iterator, Optional, Tuple, FrozenSet
+from abc import ABC
+from typing import List, Iterator, Optional, Tuple, FrozenSet, Iterable
 
 from sahyun_bot.commander_settings import Command, ResponseHook, DEFAULT_MAX_SEARCH, DEFAULT_MAX_PICK
 from sahyun_bot.elastic import CustomDLC
@@ -95,10 +96,63 @@ class Picker:
         return match.needs_picking(self.user) if self.pick_needs else match.has_pick_for(self.user)
 
 
-class Request(Command):
+class BaseRequest(Command, ABC):
     def __init__(self, **beans):
         super().__init__(**beans)
-        self.__queue: MemoryQueue[Match] = beans.get('rq')
+        self._queue: MemoryQueue[Match] = beans.get('rq')
+
+    def min_rank(self) -> UserRank:
+        return UserRank.FLWR
+
+    def _enqueue_request(self, user: User, request: Match, respond: ResponseHook) -> bool:
+        position = self._queue.add(request) if user.is_admin else self._queue.offer(request, request.by_same_user)
+        return self._validate_position(position, request, respond)
+
+    def _validate_position(self, position: int, request: Match, respond: ResponseHook) -> bool:
+        if not position:
+            return respond.to_sender(f'Already played <{request}>')
+
+        if position < 0:
+            return respond.to_sender(f'Request <{request}> already in queue position {-position}')
+
+        respond.to_sender(f'Your request for <{request}> is now in position {position}')
+        if request.is_exact:
+            if request.exact.is_official:
+                return respond.to_sender('WARNING! This song is official, so it may not be playable. Ask or try again!')
+        else:
+            respond.to_sender(f'To pick exact: {pick_format(request)}')
+
+
+class Random(BaseRequest):
+    def __init__(self, **beans):
+        super().__init__(**beans)
+
+    def execute(self, user: User, alias: str, args: str, respond: ResponseHook) -> bool:
+        with self._queue:
+            match = CustomDLC.random(args, *self.__exclusions())
+            if not match:
+                without_exclusions = CustomDLC.random_pool(query=args).count()
+                message = f'Everything already played or enqueued' if without_exclusions else f'No matches'
+                return respond.to_sender(f'{message} for <{args}>')
+
+            request = Match(user, args, match)
+            return self._enqueue_request(user, request, respond)
+
+    def __exclusions(self) -> List[int]:
+        all_ids = list(self.__ids(self._queue))
+        all_ids.extend(self.__ids(self._queue.memory()))
+        return all_ids
+
+    def __ids(self, matches: Iterable[Match]) -> Iterator[int]:
+        for match in matches:
+            if match.is_exact:
+                yield match.exact.id
+
+
+class Request(BaseRequest):
+    def __init__(self, **beans):
+        super().__init__(**beans)
+
         self.__max_search = beans.get('max_search', DEFAULT_MAX_SEARCH)
         self.__max_pick = beans.get('max_pick', DEFAULT_MAX_PICK)
 
@@ -120,25 +174,13 @@ class Request(Command):
             return respond.to_sender(f'Matches for <{args}> not playable: {unplayable}')
 
         request = Match(user, args, *playable[:self.__max_pick])
-        position = self.__queue.add(request) if user.is_admin else self.__queue.offer(request, request.by_same_user)
-        if not position:
-            return respond.to_sender(f'Already played <{request}>')
-
-        if position < 0:
-            return respond.to_sender(f'Request <{request}> already in queue position {-position}')
-
-        respond.to_sender(f'Your request for <{request}> is now in position {position}')
-        if request.is_exact:
-            if request.exact.is_official:
-                return respond.to_sender('WARNING! This song is official, so it may not be playable. Ask or try again!')
-        else:
-            respond.to_sender(f'To pick exact: {pick_format(request)}')
+        return self._enqueue_request(user, request, respond)
 
 
-class Pick(Command):
+class Pick(BaseRequest):
     def __init__(self, **beans):
         super().__init__(**beans)
-        self.__queue: MemoryQueue[Match] = beans.get('rq')
+
         self.__max_pick = beans.get('max_pick', DEFAULT_MAX_PICK)
         self.__choices = list(map(str, range(1, self.__max_pick + 1)))
 
@@ -155,13 +197,13 @@ class Pick(Command):
             return respond.to_sender(f'Try !pick 1-{self.__max_pick}')
 
         picker = Picker(user)
-        with self.__queue:
-            request = self.__queue.find(picker.needs_picking)
+        with self._queue:
+            request = self._queue.find(picker.needs_picking)
             if not request:
                 if user.is_admin:
                     return self.__pick_last(choice, respond)
 
-                request = self.__queue.find(picker.has_pick_for)
+                request = self._queue.find(picker.has_pick_for)
                 if not request:
                     return True
 
@@ -169,11 +211,11 @@ class Pick(Command):
             if not chosen:
                 return respond.to_sender(f'{choice} is not available; max: {len(request)}')
 
-            position = self.__queue.offer(chosen, picker.last_pick)
-            respond.to_sender(f'Your request for <{chosen}> is now in position {position}')
+            position = self._queue.offer(chosen, picker.last_pick)
+            return self._validate_position(position, chosen, respond)
 
     def __pick_last(self, choice: int, respond: ResponseHook) -> bool:
-        request = self.__queue.last()
+        request = self._queue.last()
         if not request:
             return respond.to_sender('Nothing to pick')
 
@@ -181,7 +223,7 @@ class Pick(Command):
         if not chosen:
             return respond.to_sender(f'{choice} is not available; max: {len(request)}')
 
-        self.__queue.mandela(chosen)
+        self._queue.mandela(chosen)
         respond.to_sender(f'Next: <{chosen}>')
 
     def __choice(self, alias: str, args: str) -> int:
